ec3ae1bb17b1d674af02fc676ac3485898f71b31
Michael <muniu.dev@gmail.com>
2023-12-13 23:31:10 +0300

    Lab Completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -7,10 +7,27 @@ function SpicyFoodList() {
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
     console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
   }
 
+  function handleLiClick(id) {
+
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }   
+  
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -24,4 +41,3 @@ function SpicyFoodList() {
 }
 
 export default SpicyFoodList;
-
